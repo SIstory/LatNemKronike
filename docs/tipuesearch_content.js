a6ba4a0053940b99bf9f24abdf242a89b5ed94d8
mihaojst <35955112+mihaojst@users.noreply.github.com>
2023-06-08 11:49:03 +0200

    dodane slike, spremenil postavitev in nivoje

---
+++ docs/tipuesearch_content.js
@@ -0,0 +1,9 @@
+var tipuesearch = {"pages": [
+                                    { "title": "1832/1", "text": "", "tags": "", "url": "a1.html#pb-1" },
+{ "title": "1832/1", "text": "1832/1", "tags": "", "url": "a1.html#head-1" },
+{ "title": "1832/1", "text": "", "tags": "", "url": "a1.html#pb-2" },
+{ "title": "1832/1", "text": "", "tags": "", "url": "a1.html#pb-3" },
+{ "title": "1833/39", "text": "Sontag 15 ten Waise und Mörder Montag 16 Königin von 16 Jahren Dienstag 17 Traum. Das war ich. Mittwoch 18 0 Donnerstag 19 Ahnfrau Freytag 20 0 Samstag 21 Onkel Adam, Nichte Eva. Tableau Sonntag 22 Lumpazius Vagabundus Montag 23 d o d o Dienstag 24 Erbvertrag Mittwoch 25 0 Donnerstag 26 Leichtsinn aus Liebe Freytag 27 0 Samstag 28 Zampa", "tags": "", "url": "a1.html#table-40" }
+
+                     ]};
+                
